b666a2848b463e44592331685334a76ddfb5ef52
Aidaarka <66637129+Aidaarka@users.noreply.github.com>
2020-12-16 12:30:17 +0300

    Add files via upload

---
+++ First.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+
+namespace Lesson6.First
+{
+    class Notorious
+    {
+        static void Main()
+        {
+            Queue notoriousQueue = new Queue();
+            Random rndCabinet = new Random();
+            for (int i = 1; ; i++)
+            {
+                Console.WriteLine("\nВведите ваше имя и фамилию, чтобы встать в очередь.");
+                notoriousQueue.Enqueue(Console.ReadLine());
+                if (notoriousQueue.Count == 6)
+                {
+                    Console.WriteLine($"\nУважаемый {notoriousQueue.Dequeue()}, пройдите в {rndCabinet.Next(1,10)} кабинет!");
+                }
+            }
+        }
+    }
+}
