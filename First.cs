11e60aa64894cfbff66811bf692cf35418da238e
Aidaarka <66637129+Aidaarka@users.noreply.github.com>
2020-12-16 12:36:13 +0300

    Add files via upload

---
+++ First.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+
+namespace Lesson6.First
+{
+    class Notary
+    {
+        static void Main()
+        {
+            Queue notaryQueue = new Queue();
+            Random rndCabinet = new Random();
+            for (int i = 1; ; i++)
+            {
+                Console.WriteLine("\nВведите ваше имя и фамилию, чтобы встать в очередь.");
+                notaryQueue.Enqueue(Console.ReadLine());
+                if (notaryQueue.Count == 6)
+                {
+                    Console.WriteLine($"\nУважаемый {notaryQueue.Dequeue()}, пройдите в {rndCabinet.Next(1,10)} кабинет!");
+                }
+            }
+        }
+    }
+}
